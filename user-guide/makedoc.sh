35bd280d39449831ce8479956f8024e25d707856
Pontus Lurcock <pont@talvi.net>
2014-04-01 18:49:42 +0200

    Some work on user guide. Mainly formatting rather than content. Now uses a custom Pandoc LaTeX template (see newly-added README for details).

--- user-guide/makedoc.sh
+++ user-guide/makedoc.sh
@@ -7,10 +7,18 @@ FORMAT=markdown
 COMMON_OPTS="-f $FORMAT --bibliography $BIB \
 -s $NAME.txt --number-sections --csl=harvard1.csl"
 
-pandoc $COMMON_OPTS \
+LATEX_OPTS="--template=template.tex \
+--include-in-header=header-includes.tex \
+--variable fontsize=11pt"
+
+pandoc $COMMON_OPTS $LATEX_OPTS \
        --latex-engine=xelatex \
-       --include-in-header=header-includes.tex \
-       --variable fontsize=11pt \
        -o $NAME.pdf
+
+## LaTeX source output can be useful for diagnosing problems.
+# 
+# pandoc $COMMON_OPTS $LATEX_OPTS \
+#        -t latex \
+#        -o $NAME.tex
 
 pandoc $COMMON_OPTS --self-contained -o $NAME.html
